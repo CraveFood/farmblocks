fb0226f6eebf27185625ef2aee04571885257ba2
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-08-11 17:34:11 -0300

    Extended storybook webpack configuration to load sass files.

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]",
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            }
+          },
+          'sass-loader'
+        ],
+        include: path.resolve(__dirname, '../')
+      }
+    ]
+  }
+};
