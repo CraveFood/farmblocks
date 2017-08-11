8e2afc19a766103b17096b97e0288c3400c8ff53
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-08-11 14:50:07 -0300

    Created storybook configuration.

---
+++ .storybook/config.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import {configure} from '@storybook/react';
+
+const req = require.context('../src/', true, /\.story\.js$/);
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename));
+}
+
+configure(loadStories, module);
