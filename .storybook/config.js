8241c6f5893a650db4d330baf6fa36ab11a75909
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-08-23 10:50:01 -0300

    Configured addon-info for storybook.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
 import {configure} from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
+
 import '../src/assets/fonts/fonticon.scss';
 import '../src/assets/fonts/lato/fontlato.scss';
 
@@ -11,3 +13,12 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+
+// addon-info
+setDefaults({
+  inline: true,
+  maxPropsIntoLine: 1,
+  maxPropObjectKeys: 10,
+  maxPropArrayLength: 10,
+  maxPropStringLength: 100,
+});
