393ab23458d93a0631228c8f58dcd5fb901de023
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-09-11 11:05:37 -0300

    Tooltip small improvements and indentation.
    Fixed upper arrow positioning and added a story (isVisible = false) for snapshot testing.

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -5,28 +5,28 @@ import classnames from 'classnames';
 import styles from './Tooltip.scss';
 
 class Tooltip extends Component {
-    render() {
-        const classes = classnames(styles.container, styles.left, {
-            [styles.hide]: !this.props.isVisible
-        });
+  render() {
+    const classes = classnames(styles.container, styles.left, {
+      [styles.hide]: !this.props.isVisible
+    });
 
-        return (
-            <div className={styles['container-anchor']}>
-                <div className={classes}>
-                    {this.props.text}
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className={styles['container-anchor']}>
+        <div className={classes}>
+          {this.props.text}
+        </div>
+      </div>
+    );
+  }
 }
 
 Tooltip.propTypes = {
-    text: PropTypes.string.isRequired,
-    isVisible: PropTypes.bool
+  text: PropTypes.string.isRequired,
+  isVisible: PropTypes.bool
 };
 
 Tooltip.defaultProps = {
-    isVisible: true,
+  isVisible: true,
 };
 
 export default Tooltip;
