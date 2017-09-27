2903ed4f5cc3b8fdbc83558dd58efc942a136d92
Fabricio c Zuardi <fabricio@fabricio.org>
2017-09-26 22:18:22 -0300

    test(snapshots): update snapshots and remove old ones
    affects: @crave/farmblocks-alert

--- packages/alert/src/index.spec.js
+++
@@ -1,9 +0,0 @@
-const React = require("react");
-const renderer = require("react-test-renderer");
-
-const Alert = require(".");
-
-it("renders correctly", () => {
-  const tree = renderer.create(<Alert>Hello World!</Alert>).toJSON();
-  expect(tree).toMatchSnapshot();
-});
