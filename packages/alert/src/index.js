e6fec3b3eac12213e4a2d6e2b7254241ebc6eaa3
Fabricio c Zuardi <fabricio@fabricio.org>
2017-09-27 12:58:37 -0300

    feat(alert-component): style the alert according to the spec, api tweaks
    affects: @crave/farmblocks-alert
    
    refactor(alert-component): many codel legibility improvements
    
    affects: @crave/farmblocks-alert

--- packages/alert/src/index.js
+++ packages/alert/src/index.js
@@ -1,48 +1,75 @@
 //@flow
-import type { StatelessFunctionalComponent, Node } from "react";
+import type { StatelessFunctionalComponent } from "react";
 import type { AlertType } from "./AlertTypes";
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import AlertTypes from "./AlertTypes";
 
-type AlertComponent = StatelessFunctionalComponent<{
-  text: string,
-  type?: AlertType,
-  onDismiss?: Function
-}>;
-const Alert: AlertComponent = ({
-  text,
-  type = AlertTypes.NEWS,
-  onDismiss = null
-}) => {
-  const buttonAttributes = onDismiss ? { onClick: onDismiss } : {};
-  const dismissButton = onDismiss ? (
-    <DefaultDismissButton {...buttonAttributes} />
-  ) : null;
-  const props = { text, dismissButton };
-  return <BaseAlert {...props} />;
+const DismissButton = ({ onClick, className }) => {
+  const buttonAttributes = onClick ? { onClick, className } : { className };
+  return <button {...buttonAttributes} />;
 };
 
-type BaseAlertComponent = StatelessFunctionalComponent<{
-  text: string,
-  dismissButton: Node
-}>;
-const BaseAlert: BaseAlertComponent = ({ text, dismissButton }) => {
-  const textElement = text ? <p>text</p> : null;
+const ThemedDismissButton = styled(DismissButton)`
+  position: absolute;
+  top: 50%;
+  margin-top: -16px;
+  right: 24px;
+  width: 32px;
+  height: 32px;
+  border-radius: 2.7px;
+  background-color: rgba(0, 0, 0, 0.16);
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.16);
+  border: solid 0.7px rgba(0, 0, 0, 0.16);
+`;
+
+const bgColors = {
+  [AlertTypes.NEWS]: "#3498db",
+  [AlertTypes.POSITIVE]: "#2ecc71",
+  [AlertTypes.NEGATIVE]: "#dd3311",
+  [AlertTypes.ATTENTION]: "#f1c618"
+};
+
+const Alert: StatelessFunctionalComponent = (props: AlertProps) => {
+  const { dismissable, onDismiss, ...other } = props;
+  const buttonAttributes = onDismiss ? { onClick: onDismiss } : {};
+  const dismissButton = dismissable ? (
+    <ThemedDismissButton {...buttonAttributes} />
+  ) : null;
+  // return <ThemedBaseAlert {...props} />;
+  const ThemedAlert = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    background-color: ${props => bgColors[props.type]};
+    box-shadow: 0 2px 2px 0 rgba(46, 204, 113, 0.16);
+    border: solid 1px rgba(0, 0, 0, 0.16);
+    min-height: ${2 * 20 + 32}px;
+
+    > p {
+      color: white;
+    }
+  `;
   return (
-    <div>
-      {textElement}
+    <ThemedAlert type={props.type}>
+      <p>{props.text}</p>
       {dismissButton}
-    </div>
+    </ThemedAlert>
   );
 };
 
-type DefaultDismissButtonComponent = StatelessFunctionalComponent<{
-  onClick: Function
-}>;
-const DefaultDismissButton: DefaultDismissButtonComponent = ({ onClick }) => {
-  const buttonAttributes = onClick ? { onClick } : {};
-  return <button {...buttonAttributes} />;
+Alert.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(Object.keys(AlertTypes)),
+  dismissable: PropTypes.bool,
+  onDismiss: PropTypes.func
+};
+Alert.defaultProps = {
+  type: AlertTypes.NEWS,
+  dismissable: true,
+  onDismiss: () => null
 };
 
 export default Alert;
--- packages/alert/src/index.story.js
+++ packages/alert/src/index.story.js
@@ -4,12 +4,20 @@ import { withInfo } from "@storybook/addon-info";
 import { action } from "@storybook/addon-actions";
 
 import Alert from ".";
-import { POSITIVE } from "./AlertTypes";
+import { POSITIVE, NEWS, NEGATIVE, ATTENTION } from "./AlertTypes";
 
 storiesOf("Alert", module)
   .add("empty", () => <Alert />)
-  .add("empty dismissable", () => <Alert dismissable />)
+  .add("empty non dismissable", () => <Alert dismissable={false} />)
   .add("dismissable with text", () => <Alert dismissable text="Foo bar" />)
   .add("dismissable with text, type positive", () => (
     <Alert dismissable type={POSITIVE} text="Foo bar" />
+  ))
+  .add("all four types", () => (
+    <div>
+      <Alert dismissable type={POSITIVE} text="Foo bar" />
+      <Alert dismissable type={NEWS} text="Foo bar" />
+      <Alert dismissable type={NEGATIVE} text="Foo bar" />
+      <Alert dismissable type={ATTENTION} text="Foo bar" />
+    </div>
   ));
