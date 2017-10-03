18e40004d36a772e776f454270875d5785ad79d6
Fabricio c Zuardi <fabricio@fabricio.org>
2017-10-03 15:27:58 -0300

    refactor(monorepo): remove leftovers from the master branch

--- src/constants/ButtonTypes.js
+++
@@ -1,6 +0,0 @@
-export const PRIMARY = 'primary';
-export const SECONDARY = 'secondary';
-export const REQUEST = 'request';
-export const NEUTRAL = 'neutral';
-export const CLOSED = 'closed';
-export const NEGATIVE = 'negative';
