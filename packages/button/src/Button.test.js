2253b6252f081fa7ab37fcfd59c6b951cf971122
Fabricio c Zuardi <fabricio@fabricio.org>
2017-10-04 15:00:18 -0300

    test(typecheck, jest): fix jest and flow errors
    affects: @crave/farmblocks-button

--- packages/button/src/Button.test.js
+++ packages/button/src/Button.test.js
@@ -3,7 +3,7 @@ import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import Button from "./Button";
-import StyledButton from "./StyledComponents/Button";
+import StyledButton from "./styledComponents/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
