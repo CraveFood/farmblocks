05a31d91e1e09e4a1c7a37b50fffc8dc36d94a57
Fabricio C Zuardi <fabricio@fabricio.org>
2017-10-09 13:21:38 -0300

    chore(jest): include ignore patterns for the covverage report

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   verbose: true,
   setupFiles: ["<rootDir>/jest.setup.js"],
+  coveragePathIgnorePatterns: ["jest.setup.js", ".story.js"],
   testPathIgnorePatterns: ["src/Storyshots.test.js"]
 };
