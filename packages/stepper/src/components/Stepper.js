eabef9a6f835ec260813695de07dc1e83dff3df0
Andre L. C. Ferreira <alcf.2099@gmail.com>
2017-11-07 17:04:05 -0200

    refactor(Stepper api): change Stepper api to receive the quantity of completed steps and set Step st
    affects: @crave/farmblocks-stepper
    
    ISSUES CLOSED: #68

--- packages/stepper/src/components/Stepper.js
+++ packages/stepper/src/components/Stepper.js
@@ -7,26 +7,43 @@ import Container from "../styledComponents/Stepper";
 import Step from "./Step";
 
 export default class Stepper extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getStatus = this.getStatus.bind(this);
+  }
+
+  getStatus(currentIndex, lastCompletedIndex) {
+    if (lastCompletedIndex >= currentIndex) {
+      return statusTypes.COMPLETED;
+    } else if (lastCompletedIndex + 1 === currentIndex) {
+      return statusTypes.CURRENT;
+    }
+
+    return statusTypes.PENDING;
+  }
+
   render() {
-    return this.props.steps.map((item, key) => (
-      <Container key={key}>
-        <Step item={item} onClick={this.props.onClick} />
+    return this.props.steps.map((value, index) => (
+      <Container key={index}>
+        <Step
+          value={value}
+          index={index}
+          status={this.getStatus(index, this.props.completedSteps - 1)}
+          onClick={this.props.onClick}
+        />
       </Container>
     ));
   }
 
   static propTypes = {
-    steps: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        status: PropTypes.oneOf(Object.keys(statusTypes)).isRequired
-      })
-    ),
-    onClick: PropTypes.func
+    steps: PropTypes.arrayOf(PropTypes.string),
+    completedSteps: PropTypes.number,
+    onClick: PropTypes.func.isRequired
   };
 
   static defaultProps = {
-    steps: []
+    steps: [],
+    completedSteps: 0
   };
 }
--- packages/stepper/src/components/Stepper.story.js
+++ packages/stepper/src/components/Stepper.story.js
@@ -3,30 +3,19 @@ import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 
-import statusTypes from "../constants/statusTypes";
-
 import Stepper from "./Stepper";
 
 storiesOf("Stepper", module).add(
   "default",
   withInfo()(() => {
     const steps = [
-      {
-        id: "1",
-        description: "Complete profile",
-        status: statusTypes.COMPLETED
-      },
-      {
-        id: "2",
-        description: "Add products",
-        status: statusTypes.CURRENT
-      },
-      {
-        id: "3",
-        description: "Start selling",
-        status: statusTypes.PENDING
-      }
+      "Complete profile",
+      "Add bank account",
+      "Connect to purchasers",
+      "Add products",
+      "Start selling"
     ];
+    const completedSteps = 2;
 
     return (
       <div
@@ -35,7 +24,11 @@ storiesOf("Stepper", module).add(
           padding: "10px 20px"
         }}
       >
-        <Stepper steps={steps} onClick={action("clicked")} />
+        <Stepper
+          steps={steps}
+          completedSteps={completedSteps}
+          onClick={action("clicked")}
+        />
       </div>
     );
   })
