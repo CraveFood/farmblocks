af9c7a9ac907b8e9b3d5e15e6b5591fee834cddb
Andre L. C. Ferreira <alcf.2099@gmail.com>
2017-11-08 16:01:14 -0200

    feat(constants): export all color constants
    affects: @crave/farmblocks-constants
    
    ISSUES CLOSED: #82

---
+++ packages/constants/src/colors.js
@@ -0,0 +1,23 @@
+export default {
+  CARBON: "#2f313a",
+  OYSTER: "#59636f",
+  SUGAR: "#f6f8f9",
+  RED_ORANGE: "#ff4411",
+  INDIGO_MILK_CAP: "#3498db",
+  BLUE_CORN: "#2980b9",
+  LETTUCE: "#2ecc71",
+  AVOCADO: "#27ae60",
+  CORN: "#f1c618",
+  BARLEY: "#ccaa00",
+  PUMPKIN: "#e67e22",
+  CARROT: "#d35400",
+  CARAMEL: "#9e5500",
+  COFFEE: "#774411",
+  STRAWBERRY: "#dd3311",
+  TOMATO: "#bc2b0e",
+  GRAPE: "#9b59b6",
+  EGGPLANT: "#8e44ad",
+  GREY_16: "rgba(0, 0, 0, 0.16)",
+  GREY_32: "rgba(0, 0, 0, 0.32)",
+  WHITE_56: "rgba(255, 255, 255, 0.56)"
+};
