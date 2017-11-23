739ea27b2e43c91eedce5325dec3efea24dbbd45
Felipe Gameiro <felipegameiro@gmail.com>
2017-11-23 15:38:13 -0200

    style: add hoverColors file
    affects: @crave/farmblocks-link

---
+++ packages/link/src/constants/hoverColors.js
@@ -0,0 +1,10 @@
+import { colors as colorConstants } from "@crave/farmblocks-theme";
+
+import linkTypes from "./linkTypes";
+
+export default {
+  [linkTypes.FEATURED]: colorConstants.BLUE_CORN,
+  [linkTypes.NEUTRAL]: colorConstants.INDIGO_MILK_CAP,
+  [linkTypes.NORMAL]: colorConstants.BLUE_CORN,
+  [linkTypes.WHITE]: colorConstants.WHITE_56
+};
