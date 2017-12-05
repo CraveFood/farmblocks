8ca1ef53a690984ca4d44f208b04a00c2334ee8f
Fabricio C Zuardi <fabricio@fabricio.org>
2017-12-05 16:01:57 -0200

    feat(properties): accept value and onChange properties
    affects: @crave/farmblocks-input-text
    
    making this input a Controlled Component

--- packages/input-text/src/TextInput.js
+++ packages/input-text/src/TextInput.js
@@ -6,19 +6,40 @@ import StyledLabel from "./styledComponents/TextInput";
 class TextInput extends React.Component {
   static propTypes = {
     label: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
 
     placeholder: PropTypes.string
     // and any other properties to forward to the html input element...
   };
 
+  static defaultProps = {
+    value: "",
+    onChange: () => null
+  };
+
   constructor(props) {
     super(props);
+    this.state = {
+      value: this.props.value
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ value: event.target.value });
+    return this.props.onChange(event);
   }
 
   render() {
-    const { label, ...otherProps } = this.props;
+    const { label, value, onChange, ...otherProps } = this.props;
     const containerProps = {};
-    const inputProps = { ...otherProps };
+    const inputProps = {
+      value: this.state.value,
+      onChange: this.onChange,
+      ...otherProps
+    };
     return (
       <StyledLabel {...containerProps}>
         {this._renderLabel(label)}
--- packages/input-text/src/TextInput.story.js
+++ packages/input-text/src/TextInput.story.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 
@@ -11,5 +12,25 @@ storiesOf("Text Input", "module")
     "With placeholder",
     withInfo()(() => (
       <TextInput label="Field Label" placeholder="input value here" />
+    ))
+  )
+  .add(
+    "With value",
+    withInfo()(() => (
+      <TextInput
+        value="a value"
+        label="Field Label"
+        placeholder="input value here"
+      />
+    ))
+  )
+  .add(
+    "With change handler",
+    withInfo()(() => (
+      <TextInput
+        onChange={action("input changed")}
+        label="Field Label"
+        placeholder="input value here"
+      />
     ))
   );
