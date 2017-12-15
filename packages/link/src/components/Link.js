60ea799ec02223b6ece15d2eb56768cfcf5ef1bd
Fabricio C Zuardi <fabricio@fabricio.org>
2017-12-15 01:28:22 -0200

    refactor(link): use disabled tooltip hoc on the link
    affects: @crave/farmblocks-link
    
    removed the onClick wrapper as well, not really sure why it used the event.stopPropagation
    
    BREAKING CHANGE:
    previous versions wrapped the onclick event in a function that had event.stopPropagation, on this
    version, onClick is forwarded to the a tag as is

--- packages/link/src/components/Link.js
+++ packages/link/src/components/Link.js
@@ -1,93 +1,66 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import Tooltip from "@crave/farmblocks-tooltip";
 import { fontSizes } from "@crave/farmblocks-theme";
+import disabledTooltip, {
+  newPropTypes
+} from "@crave/farmblocks-hoc-disabled-tooltip";
 
 import linkTypes from "../constants/linkTypes";
-import Container from "../styledComponents/Link";
+import StyledLink from "../styledComponents/Link";
 
-export default class Link extends React.Component {
-  constructor(props) {
-    super(props);
+const Container = disabledTooltip(StyledLink);
 
-    this.state = {
-      showTooltip: false
-    };
-
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseLeaves = this.mouseLeaves.bind(this);
-    this.onClick = this.onClick.bind(this);
-  }
-
-  render() {
-    const { children, ...props } = this.props;
-    return (
-      <Container {...props}>
-        {props.leftIcon && <i className={`${props.leftIcon} margin-right }`} />}
-
-        {props.disabled ? (
-          <span onMouseOver={this.mouseOver} onMouseLeave={this.mouseLeaves}>
-            {children}
-          </span>
-        ) : (
-          <a href={props.href} onClick={this.onClick}>
-            {children}
-          </a>
-        )}
-
-        {props.external && <i className="wg-external-link margin-left" />}
-
-        <Tooltip
-          text={props.tooltipText}
-          isVisible={this.state.showTooltip}
-          align={props.tooltipAlign}
-        />
-      </Container>
-    );
-  }
-
-  onClick(e) {
-    e.stopPropagation();
-    return this.props.onClick(e);
-  }
-
-  mouseOver() {
-    if (this.props.disabled) {
-      this.setState({
-        showTooltip: true
-      });
-    }
-  }
-
-  mouseLeaves() {
-    if (this.props.disabled) {
-      this.setState({
-        showTooltip: false
-      });
-    }
-  }
-
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-    href: PropTypes.string,
-    type: PropTypes.oneOf(Object.values(linkTypes)),
-    tooltipAlign: PropTypes.oneOf(["left", "right"]),
-    tooltipText: PropTypes.string,
-    onClick: PropTypes.func,
-    leftIcon: PropTypes.string,
-    disabled: PropTypes.bool,
-    external: PropTypes.bool,
-    size: PropTypes.number,
-    lineHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+const Link = props => {
+  const {
+    size,
+    lineHeight,
+    type,
+    disabled,
+    tooltipText,
+    tooltipAlign,
+    displayBlock,
+    leftIcon,
+    external,
+    children,
+    ...linkProps
+  } = props;
+  const containerProps = {
+    size,
+    lineHeight,
+    type,
+    disabled,
+    tooltipText,
+    tooltipAlign,
+    displayBlock
   };
+  return (
+    <Container {...containerProps}>
+      {leftIcon && <i className={`${leftIcon} margin-right }`} />}
+      <a {...linkProps}>{children}</a>
+      {external && <i className="wg-external-link margin-left" />}
+    </Container>
+  );
+};
 
-  static defaultProps = {
-    type: linkTypes.FEATURED,
-    tooltipAlign: "left",
-    tooltipText: "This link is disabled",
-    size: fontSizes.MEDIUM,
-    lineHeight: 1,
-    disabled: false,
-    external: false
-  };
-}
+Link.propTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(Object.values(linkTypes)),
+  leftIcon: PropTypes.string,
+  external: PropTypes.bool,
+  size: PropTypes.number,
+  lineHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  ...newPropTypes
+  //... and all properties of html <a>
+};
+
+Link.defaultProps = {
+  type: linkTypes.FEATURED,
+  tooltipAlign: "left",
+  tooltipText: "This link is disabled",
+  size: fontSizes.MEDIUM,
+  lineHeight: 1,
+  disabled: false,
+  external: false
+};
+
+export default Link;
