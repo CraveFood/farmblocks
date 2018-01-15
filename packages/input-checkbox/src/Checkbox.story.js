4282322919dcd5affc6f1af3eaa7cb6ca1fd5091
Fabricio C Zuardi <fabricio@fabricio.org>
2018-01-15 14:18:57 -0200

    test(stories): basic stories for checkbox input
    affects: @crave/farmblocks-input-checkbox
    
    disabled, with label and on a group
    
    squash! test(stories): basic stories for checkbox input

--- packages/input-checkbox/src/Checkbox.story.js
+++ packages/input-checkbox/src/Checkbox.story.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { withInfo } from "@storybook/addon-info";
 
 import Checkbox from ".";
 
-storiesOf("Checkbox Input", "module").add(
-  "Default",
-  withInfo()(() => <Checkbox />)
-);
+storiesOf("Checkbox Input", "module")
+  .add("Default", withInfo()(() => <Checkbox />))
+  .add("checked", withInfo()(() => <Checkbox checked />))
+  .add("with text label", withInfo()(() => <Checkbox label="Checkbox Label" />))
+  .add(
+    "with onChange event",
+    withInfo()(() => (
+      <Checkbox label="Checkbox Label" onChange={action("onChange")} />
+    ))
+  )
+  .add(
+    "disabled",
+    withInfo()(() => (
+      <Checkbox
+        disabled
+        tooltipText="custom disabled text"
+        checked
+        label="Checkbox Label"
+      />
+    ))
+  )
+  .add(
+    "in a fieldset",
+    withInfo()(() => (
+      <fieldset>
+        <Checkbox label="Checkbox Label 1" name="foo" />
+        <Checkbox disabled label="Checkbox Label 2" name="foo" />
+        <Checkbox label="Checkbox Label 3" name="foo" />
+      </fieldset>
+    ))
+  );
