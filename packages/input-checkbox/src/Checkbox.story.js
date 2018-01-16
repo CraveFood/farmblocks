f23a05d6120aa1d52e87bfb44fbd9905b363dee2
Fabricio C Zuardi <fabricio@fabricio.org>
2018-01-16 19:21:19 -0200

    test(stories): add a disabled checked checkbox to the fieldset example
    affects: @crave/farmblocks-input-checkbox

--- packages/input-checkbox/src/Checkbox.story.js
+++ packages/input-checkbox/src/Checkbox.story.js
@@ -31,7 +31,13 @@ storiesOf("Checkbox Input", "module")
     withInfo()(() => (
       <fieldset>
         <Checkbox label="Checkbox Label 1" name="foo" />
-        <Checkbox disabled label="Checkbox Label 2" name="foo" />
+        <Checkbox disabled label="Disabled Checkbox" name="foo" />
+        <Checkbox
+          checked
+          disabled
+          label="Checked Disabled Checkbox"
+          name="foo"
+        />
         <Checkbox label="Checkbox Label 3" name="foo" />
         <div style={{ marginTop: 24 }}>
           <label>
