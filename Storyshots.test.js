e15f535ad064550931702a6f7fe78f080c802797
Fabricio C Zuardi <fabricio@fabricio.org>
2018-01-17 17:20:55 -0200

    fix(storybook): apply a workaround for storybook bug https://github.com/storybooks/storybook/issues/

--- Storyshots.test.js
+++ Storyshots.test.js
@@ -1,2 +1,13 @@
 import initStoryshots from "@storybook/addon-storyshots";
+// Workaround for https://github.com/storybooks/storybook/issues/2509
+jest.mock("react-dom", () => {
+  return {
+    render: () => null,
+    unmountComponentAtNode: () => null,
+    findDOMNode: () => {
+      return {};
+    }
+  };
+});
+
 initStoryshots();
