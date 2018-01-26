2d26c6b7c02953275bcd4cd977a7347e7293445d
Fabricio C Zuardi <fabricio@fabricio.org>
2018-01-26 17:34:40 -0200

    fix(test): minor text change
    affects: @crave/farmblocks-table

--- packages/table/src/Column.test.js
+++ packages/table/src/Column.test.js
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer";
 import Column from "./Column";
 
 describe("Column", function() {
-  test("Column component by itself shouldnt render nothing", function() {
+  test("Column component by itself shouldn't render anything", function() {
     const component = renderer.create(<Column />);
     expect(component.toJSON()).toBeNull();
   });
