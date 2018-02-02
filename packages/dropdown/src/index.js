0469a73eaee2b5fa930c9f73e35ac92754dbca5f
André L. C. Ferreira <alcf.2099@gmail.com>
2018-02-02 14:10:25 -0200

    feat(dropdown): export dropdown wrappers
    affects: @crave/farmblocks-dropdown
    
    ISSUES CLOSED: #230

--- packages/dropdown/src/index.js
+++ packages/dropdown/src/index.js
@@ -1,2 +1,9 @@
 export { default as Dropdown } from "./components/Dropdown";
 export { default as DropdownItem } from "./components/DropdownItem";
+export { default as DropdownWrapper } from "./styledComponents/DropdownWrapper";
+export {
+  default as DropdownMenuWrapper
+} from "./styledComponents/DropdownMenuWrapper";
+export {
+  default as DropdownItemWrapper
+} from "./styledComponents/DropdownItemWrapper";
