83a1f7a14df56bc9606b2a217c676f826792ed50
Vinicius Nakamura <hello@vnakamura.com>
2018-03-08 12:19:01 -0300

    feat(animation): fade balloon on open
    affects: @crave/farmblocks-map-balloon

--- packages/map-balloon/src/styledComponents/Balloon.js
+++ packages/map-balloon/src/styledComponents/Balloon.js
@@ -1,20 +1,39 @@
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import Card from "@crave/farmblocks-card";
 
 import alignments from "../constants/alignments";
 
 const alignStyles = {
-  [alignments.LEFT]: css`left: -33px;`,
-  [alignments.RIGHT]: css`right: -33px;`
+  [alignments.LEFT]: css`
+    left: -33px;
+    transform-origin: bottom left;
+  `,
+  [alignments.RIGHT]: css`
+    transform-origin: bottom right;
+    right: -33px;
+  `
 };
 
+const open = keyframes`
+  from {
+    transform: scale(.5);
+    opacity: 0;
+  }
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+`;
+
 const Balloon = styled(Card)`
+  animation: ${open} 0.3s ease-in-out;
   position: absolute;
   bottom: 17px;
   ${props => alignStyles[props.align]} height: 320px;
   width: 320px;
   padding: 0;
   border-radius: 8px;
+  border: none;
   display: flex;
   flex-direction: column;
 `;
