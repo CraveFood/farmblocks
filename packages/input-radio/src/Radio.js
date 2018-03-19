7e7afeb4817600ca3cc8cb9968be063514a3d90f
Vinicius Nakamura <hello@vnakamura.com>
2018-03-19 17:34:13 -0300

    feat(radio): add onClick behavior
    affects: @crave/farmblocks-input-radio

--- packages/input-radio/src/Radio.js
+++ packages/input-radio/src/Radio.js
@@ -5,29 +5,60 @@ import { fontSizes, fontTypes } from "@crave/farmblocks-theme";
 
 import Label from "./styledComponents/Label";
 
-const Radio = ({ label, ...inputProps }) => {
-  const fontColor =
-    (inputProps.disabled && fontTypes.SUBTLE) || fontTypes.NORMAL;
-  const labelProps = {
-    checked: inputProps.checked,
-    disabled: inputProps.disabled
+class Radio extends React.Component {
+  state = {
+    checked: this.props.checked
   };
-  return (
-    <Label {...labelProps}>
-      <input className="hiddenInput" type="radio" {...inputProps} />
-      <div className="visibleInput">
-        <div className="checked" />
-      </div>
-      {label && (
-        <Text title type={fontColor} size={fontSizes.MEDIUM}>
-          {label}
-        </Text>
-      )}
-    </Label>
-  );
-};
-Radio.propTypes = {
-  label: PropTypes.string
-};
+
+  handleClick = event => {
+    event.preventDefault();
+    const { disabled, onClick } = this.props;
+    !disabled &&
+      this.setState({
+        checked: true
+      });
+    onClick && onClick(event);
+  };
+
+  render() {
+    const { label, onClick, ...inputProps } = this.props;
+    const { checked } = this.state;
+
+    const labelProps = {
+      checked,
+      onClick: this.handleClick,
+      disabled: inputProps.disabled
+    };
+    inputProps.checked = checked;
+
+    const fontColor =
+      (inputProps.disabled && fontTypes.SUBTLE) || fontTypes.NORMAL;
+
+    return (
+      <Label {...labelProps}>
+        <input className="hiddenInput" type="radio" {...inputProps} />
+        <div className="visibleInput">
+          <div className="checked" />
+        </div>
+        {label && (
+          <Text title type={fontColor} size={fontSizes.MEDIUM}>
+            {label}
+          </Text>
+        )}
+      </Label>
+    );
+  }
+
+  static propTypes = {
+    label: PropTypes.string,
+    checked: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func
+  };
+
+  static defaultProps = {
+    checked: false
+  };
+}
 
 export default Radio;
--- packages/input-radio/src/Radio.story.js
+++ packages/input-radio/src/Radio.story.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { withInfo } from "@storybook/addon-info";
 
 import Radio from ".";
 
 storiesOf("Radio", "module")
-  .add("Default", withInfo()(() => <Radio label="Default radio" />))
-  .add("Checked", withInfo()(() => <Radio label="Checked radio" checked />))
-  .add("Disabled", withInfo()(() => <Radio label="Disabled radio" disabled />));
+  .add("Default", withInfo()(() => <Radio />))
+  .add("Checked", withInfo()(() => <Radio checked />))
+  .add("Disabled", withInfo()(() => <Radio disabled />))
+  .add("Default w/ label", withInfo()(() => <Radio label="Default radio" />))
+  .add(
+    "Checked w/ label",
+    withInfo()(() => <Radio label="Checked radio" checked />)
+  )
+  .add(
+    "Disabled w/ label",
+    withInfo()(() => <Radio label="Disabled radio" disabled />)
+  )
+  .add(
+    "onClick",
+    withInfo()(() => <Radio label="Click me" onClick={action("onClick")} />)
+  );
