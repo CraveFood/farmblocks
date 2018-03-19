6ce6dbb284a06ddf799036db81f20a0055444a47
Vinicius Nakamura <hello@vnakamura.com>
2018-03-19 15:46:47 -0300

    feat(radio): add input and Text as label
    affects: @crave/farmblocks-input-radio

--- packages/input-radio/src/Radio.js
+++ packages/input-radio/src/Radio.js
@@ -1,7 +1,22 @@
-import * as React from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import Text from "@crave/farmblocks-text";
+import { fontSizes, fontTypes } from "@crave/farmblocks-theme";
 
-const Radio = props => {
-  return <div />;
+const Radio = ({ label, ...inputProps }) => {
+  const fontColor =
+    (inputProps.disabled && fontTypes.SUBTLE) || fontTypes.NORMAL;
+  return (
+    <label>
+      <input className="hidden-input" type="radio" {...inputProps} />
+      <Text type={fontColor} size={fontSizes.MEDIUM}>
+        {label}
+      </Text>
+    </label>
+  );
+};
+Radio.propTypes = {
+  label: PropTypes.string
 };
 
 export default Radio;
--- packages/input-radio/src/Radio.story.js
+++ packages/input-radio/src/Radio.story.js
@@ -4,4 +4,7 @@ import { withInfo } from "@storybook/addon-info";
 
 import Radio from ".";
 
-storiesOf("Radio", "module").add("Default", withInfo()(() => <Radio />));
+storiesOf("Radio", "module")
+  .add("Default", withInfo()(() => <Radio label="Default radio" />))
+  .add("Checked", withInfo()(() => <Radio label="Checked radio" checked />))
+  .add("Disabled", withInfo()(() => <Radio label="Disabled radio" disabled />));
