005716f8151090058ce3416d8e716b7734543391
Fabricio C Zuardi <fabricio@fabricio.org>
2018-03-26 16:38:29 -0300

    chore(jest): run the complete test suite only on some cases
    like on CI, or when generating the snapshots or generating coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,19 @@
-module.exports = {
+const { argv } = process;
+const coveragePathIgnorePatterns = [".story.js", "lib/*"];
+
+const completeConfig = {
   verbose: true,
-  setupTestFrameworkScriptFile: "<rootDir>/jest.testFramework.js",
-  coveragePathIgnorePatterns: [".story.js", "lib/*"],
-  testPathIgnorePatterns: ["src/Storyshots.test.js"]
+  coveragePathIgnorePatterns,
+  setupTestFrameworkScriptFile: "<rootDir>/jest.testFramework.js"
 };
+
+const fastConfig = {
+  verbose: false,
+  onlyChanged: true,
+  coveragePathIgnorePatterns,
+  testPathIgnorePatterns: ["Storyshots.test.js"]
+};
+
+const useComplete = argv.includes("-u") || argv.includes("--coverage");
+
+module.exports = useComplete ? completeConfig : fastConfig;
