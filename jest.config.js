0264ae5ba3e67f5f3ac520144ceb3699e1140ddf
Fabricio C Zuardi <fabricio@fabricio.org>
2018-03-26 15:32:58 -0300

    refactor(jest): remove raf workaround that is no longer needed

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   verbose: true,
-  setupFiles: ["<rootDir>/jest.setup.js"],
   setupTestFrameworkScriptFile: "<rootDir>/jest.testFramework.js",
-  coveragePathIgnorePatterns: ["jest.setup.js", ".story.js", "lib/*"],
+  coveragePathIgnorePatterns: [".story.js", "lib/*"],
   testPathIgnorePatterns: ["src/Storyshots.test.js"]
 };
