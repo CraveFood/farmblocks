f2997e9a44bc541447e231dd40e36662c8a5e282
André L. C. Ferreira <alcf.2099@gmail.com>
2018-04-12 16:05:56 -0300

    feat(DropdownWrapper): add displayBlock property
    affects: @crave/farmblocks-dropdown
    
    ISSUES CLOSED: #332

--- packages/dropdown/src/styledComponents/DropdownWrapper.js
+++ packages/dropdown/src/styledComponents/DropdownWrapper.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-export default styled.div`
+const DropdownWrapper = styled.div`
   font-family: lato, sans-serif;
 
   *:focus {
     outline: none;
   }
 
-  display: inline-block;
+  display: ${props => (props.displayBlock ? "block" : "inline-block")};
   position: relative;
 `;
+
+DropdownWrapper.propTypes = {
+  displayBlock: PropTypes.bool
+};
+
+export default DropdownWrapper;
