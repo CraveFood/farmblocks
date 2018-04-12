fd39eea20a1182cf5d8307869f4baa767515690c
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-12 17:00:46 -0300

    fix(snapshots): make jest-styled-components work with multi snapshots

--- Storyshots.test.js
+++ Storyshots.test.js
@@ -1,6 +1,13 @@
 import initStoryshots, {
   multiSnapshotWithOptions
 } from "@storybook/addon-storyshots";
+import styleSheetSerializer from "jest-styled-components/src/styleSheetSerializer";
+import { addSerializer } from "jest-specific-snapshot";
+
+//add jest-styled-components serializer in order for jest-styled-components to work
+// see https://github.com/storybooks/storybook/issues/887#issuecomment-356577444
+// and https://github.com/storybooks/storybook/issues/887#issuecomment-357073807
+addSerializer(styleSheetSerializer);
 
 //mock withInfo to exclude info HTML from the snapshots
 jest.mock("@storybook/addon-info", () => ({
