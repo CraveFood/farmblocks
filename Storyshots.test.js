b7ad320e7c13d8e75357188604f797ce60162666
Fabricio C Zuardi <fabricio@fabricio.org>
2018-04-12 11:58:31 -0300

    refactor(snapshots config): remove old workaround

--- Storyshots.test.js
+++ Storyshots.test.js
@@ -1,13 +1,3 @@
 import initStoryshots from "@storybook/addon-storyshots";
-// Workaround for https://github.com/storybooks/storybook/issues/2509
-jest.mock("react-dom", () => {
-  return {
-    render: () => null,
-    unmountComponentAtNode: () => null,
-    findDOMNode: () => {
-      return {};
-    }
-  };
-});
 
 initStoryshots();
