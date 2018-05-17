7082687f30c2fecd8e70daa17052ad35587d3ddf
Felipe Gameiro <felipegameiro@gmail.com>
2018-05-17 16:24:10 -0300

    (Amount selector): initial component api
    affects: @crave/farmblocks-amount-selectors
    
    WIP(amount-selector): more stories
    
    affects: @crave/farmblocks-amount-selectors

--- packages/amount-selectors/src/components/AmountSelectors.js
+++ packages/amount-selectors/src/components/AmountSelectors.js
@@ -1,7 +1,63 @@
 import * as React from "react";
+import PropTypes from "prop-types";
+import Button, { buttonTypes, buttonSizes } from "@crave/farmblocks-button";
+import InputText from "@crave/farmblocks-input-text";
 
-const AmountSelectors = props => {
-  return <div />;
-};
+class AmountSelectors extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    };
+  }
+
+  decrement = () => {
+    this.setState({
+      value: Math.max(this.state.value - this.props.step, this.props.minValue)
+    });
+  };
+
+  increment = () => {
+    this.setState({
+      value: Math.min(this.state.value + this.props.step, this.props.maxValue)
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button
+          type={buttonTypes.SECONDARY}
+          size={buttonSizes.MEDIUM}
+          icon="wg-minus"
+          onClick={this.decrement}
+        />
+        <div className="inputContainer">
+          <InputText value={this.state.value} />
+        </div>
+        <Button
+          type={buttonTypes.SECONDARY}
+          size={buttonSizes.MEDIUM}
+          icon="wg-add"
+          onClick={this.increment}
+        />
+      </div>
+    );
+  }
+
+  static propTypes = {
+    value: PropTypes.number,
+    step: PropTypes.number,
+    minValue: PropTypes.number,
+    maxValue: PropTypes.number
+  };
+
+  static defaultProps = {
+    value: 0,
+    step: 1,
+    minValue: 0,
+    maxValue: Number.MAX_VALUE
+  };
+}
 
 export default AmountSelectors;
--- packages/amount-selectors/src/components/AmountSelectors.story.js
+++ packages/amount-selectors/src/components/AmountSelectors.story.js
@@ -4,7 +4,14 @@ import { withInfo } from "@storybook/addon-info";
 
 import AmountSelectors from "./AmountSelectors";
 
-storiesOf("Amount selectors", "module").add(
-  "Default",
-  withInfo()(() => <AmountSelectors />)
-);
+storiesOf("Amount selectors", "module")
+  .add("Default", withInfo()(() => <AmountSelectors />))
+  .add("With value set", withInfo()(() => <AmountSelectors value={2} />))
+  .add(
+    "With 0.5 steps",
+    withInfo()(() => <AmountSelectors value={2} step={0.5} />)
+  )
+  .add(
+    "With a maxValue of 3",
+    withInfo()(() => <AmountSelectors maxValue={3} value={2} step={0.5} />)
+  );
