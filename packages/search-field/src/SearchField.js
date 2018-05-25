c8ffd45f6afa08a79a3d437113fddf150f1f60eb
Vinicius Nakamura <hello@vnakamura.com>
2018-05-25 15:17:14 -0300

    feat(search-field): add propTypes and defaultProps
    affects: @crave/farmblocks-search-field

--- packages/search-field/src/SearchField.js
+++ packages/search-field/src/SearchField.js
@@ -1,7 +1,52 @@
 import * as React from "react";
+import PropTypes from "prop-types";
+import { compose } from "recompose";
+import disabledTooltip, {
+  disabledTooltipProps
+} from "@crave/farmblocks-hoc-disabled-tooltip";
+import formInput, { formInputProps } from "@crave/farmblocks-hoc-input";
+import withMessages, {
+  withMessagesProps
+} from "@crave/farmblocks-hoc-validation-messages";
 
-const SearchField = props => {
-  return <div />;
-};
+const EnhancedInput = compose(disabledTooltip, withMessages, formInput)(
+  "input"
+);
+EnhancedInput.displayName = "EnhancedInput";
+
+class SearchField extends React.Component {
+  render() {
+    const { onChange } = this.props;
+    return <EnhancedInput onChange={onChange} type="search" />;
+  }
+
+  static defaultProps = {
+    onChange: () => false,
+    onSelect: () => false,
+    width: "200px",
+    maxMenuHeight: "353px"
+  };
+
+  static propTypes = {
+    items: PropTypes.arrayOf(
+      PropTypes.oneOfType([
+        PropTypes.shape({
+          value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+          label: PropTypes.string,
+          image: PropTypes.string
+        }),
+        PropTypes.node
+      ])
+    ),
+    loading: PropTypes.node,
+    width: PropTypes.string,
+    maxMenuHeight: PropTypes.string,
+    onChange: PropTypes.func,
+    onSelect: PropTypes.func,
+    ...formInputProps,
+    ...withMessagesProps,
+    ...disabledTooltipProps
+  };
+}
 
 export default SearchField;
