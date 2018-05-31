2c45704bdeff9f998f0995915e671a2ee1eaa973
Fabricio C Zuardi <fabricio@fabricio.org>
2018-05-31 09:50:24 -0300

    test(amount-selector): new story to test props received after mount
    affects: @crave/farmblocks-amount-selectors

--- packages/amount-selectors/src/components/AmountSelectors.story.js
+++ packages/amount-selectors/src/components/AmountSelectors.story.js
@@ -10,6 +10,25 @@ storiesOf("Amount selectors", module)
   .add("Default", withInfo()(() => <AmountSelectors />))
   .add("With value set", withInfo()(() => <AmountSelectors value={2} />))
   .add(
+    "With value set after mount",
+    withInfo()(() => {
+      class MyStory extends React.Component {
+        state = { value: 10 };
+        render() {
+          return (
+            <div>
+              <AmountSelectors value={this.state.value} />
+              <button onClick={() => this.setState({ value: Math.random() })}>
+                Set Value
+              </button>
+            </div>
+          );
+        }
+      }
+      return <MyStory />;
+    })
+  )
+  .add(
     "With 0.5 steps",
     withInfo()(() => <AmountSelectors value={2} step={0.5} />)
   )
