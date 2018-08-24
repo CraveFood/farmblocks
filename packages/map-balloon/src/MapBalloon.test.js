2f85ad952b270d3a3a8c71617676e27d14126f91
Vinicius Nakamura <hello@vnakamura.com>
2018-08-24 14:25:35 -0300

    test(map-balloon): add tests for onPinClick and onBalloonClick
    affects: @crave/farmblocks-map-balloon

--- packages/map-balloon/src/MapBalloon.test.js
+++ packages/map-balloon/src/MapBalloon.test.js
@@ -1,6 +1,67 @@
-import { requiredIfNoSingleImage } from "./MapBalloon";
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme, { shallow } from "enzyme";
+
+import MapBalloon, { requiredIfNoSingleImage } from "./MapBalloon";
+import Pin from "./styledComponents/Pin";
+import Balloon from "./styledComponents/Balloon";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("MapBalloon", () => {
+  describe("click events", () => {
+    let reference, event, onPinClickSpy, onBalloonClickSpy, commonProps;
+    beforeEach(() => {
+      reference = "some Id";
+      event = {};
+      onPinClickSpy = jest.fn();
+      onBalloonClickSpy = jest.fn();
+      commonProps = {
+        reference,
+        x: 400,
+        y: 400,
+        caption: "Paloma Orchards",
+        imageSet: [
+          {
+            image: "https://picsum.photos/640/?random",
+            name: "Romaine Lettuce"
+          }
+        ]
+      };
+    });
+    afterEach(() => {
+      reference = null;
+      event = null;
+      onPinClickSpy.mockReset();
+      onBalloonClickSpy.mockReset();
+      commonProps = null;
+    });
+
+    describe("onPinClick", () => {
+      test("should pass the reference", () => {
+        const wrapper = shallow(
+          <MapBalloon onPinClick={onPinClickSpy} {...commonProps} />
+        );
+        wrapper.find(Pin).simulate("click", event);
+
+        expect(onPinClickSpy).toHaveBeenCalledTimes(1);
+        expect(onPinClickSpy).toHaveBeenCalledWith(reference, event);
+      });
+    });
+
+    describe("onBalloonClick", () => {
+      test("should pass the reference", () => {
+        const wrapper = shallow(
+          <MapBalloon onBalloonClick={onBalloonClickSpy} {...commonProps} />
+        );
+        wrapper.find(Balloon).simulate("click", event);
+
+        expect(onBalloonClickSpy).toHaveBeenCalledTimes(1);
+        expect(onBalloonClickSpy).toHaveBeenCalledWith(reference, event);
+      });
+    });
+  });
+
   describe("requiredIfNoSingleImage", () => {
     test("prop should be optional if singleImage exists", () => {
       const propTypeMock = jest.fn();
