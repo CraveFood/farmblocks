2556280ddfb732e15c5ddc853e46eca41f2c9de2
Fabricio C Zuardi <fabricio@fabricio.org>
2018-09-13 14:58:18 -0300

    test(form-wrapper): stories
    affects: @crave/farmblocks-form-wrapper

--- packages/form-popover/src/FormPopover.story.js
+++ packages/form-popover/src/FormPopover.story.js
@@ -1,10 +1,32 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+import Input from "@crave/farmblocks-input-text";
 
 import FormPopover from ".";
 
-storiesOf("Form Popover", module).add(
-  "Default",
-  withInfo()(() => <FormPopover />)
-);
+storiesOf("Form Popover", module)
+  .add("Default", withInfo()(() => <FormPopover />))
+  .add("With title", withInfo()(() => <FormPopover title="My form" />))
+  .add(
+    "With form content and save handler",
+    withInfo()(() => {
+      let formData = new window.FormData();
+      formData.set("fieldA", "foo");
+      formData.set("fieldB", "bar");
+      const content = data => (
+        <div>
+          <Input label="Field Label" name="fieldA" value={data.get("fieldA")} />
+          <Input label="Field Label" bame="fieldB" value={data.get("fieldB")} />
+        </div>
+      );
+      const props = {
+        formData,
+        content,
+        onSave: action("saved"),
+        title: "Action Name"
+      };
+      return <FormPopover {...props} />;
+    })
+  );
