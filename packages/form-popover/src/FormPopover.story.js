8b5aaa114b6b3b33cdc127775c90f5c7ab6a2d84
André L. C. Ferreira <alcf.2099@gmail.com>
2018-09-14 11:03:34 -0300

    test(form-wrapper): refactored desired api on storybook
    affects: @crave/farmblocks-form-wrapper

--- packages/form-popover/src/FormPopover.story.js
+++ packages/form-popover/src/FormPopover.story.js
@@ -7,26 +7,53 @@ import Input from "@crave/farmblocks-input-text";
 import FormPopover from ".";
 
 storiesOf("Form Popover", module)
-  .add("Default", withInfo()(() => <FormPopover />))
-  .add("With title", withInfo()(() => <FormPopover title="My form" />))
   .add(
-    "With form content and save handler",
-    withInfo()(() => {
-      let formData = new window.FormData();
-      formData.set("fieldA", "foo");
-      formData.set("fieldB", "bar");
-      const content = data => (
-        <div>
-          <Input label="Field Label" name="fieldA" value={data.get("fieldA")} />
-          <Input label="Field Label" bame="fieldB" value={data.get("fieldB")} />
-        </div>
-      );
-      const props = {
-        formData,
-        content,
-        onSave: action("saved"),
-        title: "Action Name"
-      };
-      return <FormPopover {...props} />;
-    })
+    "Default",
+    withInfo()(() => (
+      <FormPopover onCancel={action("onCancel")} onSave={action("onSave")}>
+        <Input label="First Name" />
+        <Input label="Last Name" />
+      </FormPopover>
+    ))
+  )
+  .add(
+    "With title",
+    withInfo()(() => (
+      <FormPopover
+        onCancel={action("onCancel")}
+        onSave={action("onSave")}
+        title="Title"
+      >
+        <Input label="First Name" />
+        <Input label="Last Name" />
+      </FormPopover>
+    ))
+  )
+  .add(
+    "loading",
+    withInfo()(() => (
+      <FormPopover
+        onCancel={action("onCancel")}
+        onSave={action("onSave")}
+        title="Title"
+        loading
+      >
+        <Input label="First Name" />
+        <Input label="Last Name" />
+      </FormPopover>
+    ))
+  )
+  .add(
+    "extra content",
+    withInfo()(() => (
+      <FormPopover
+        onCancel={action("onCancel")}
+        onSave={action("onSave")}
+        title="Title"
+        extraContent={<div>Extra content renders after save button</div>}
+      >
+        <Input label="First Name" />
+        <Input label="Last Name" />
+      </FormPopover>
+    ))
   );
