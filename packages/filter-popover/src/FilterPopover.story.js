589d1f4246eee9b59aeb54ef931f2c9a30578582
André L. C. Ferreira <alcf.2099@gmail.com>
2018-09-14 17:00:42 -0300

    feat(FilterPopover): created component
    affects: @crave/farmblocks-filter-popover

--- packages/filter-popover/src/FilterPopover.story.js
+++ packages/filter-popover/src/FilterPopover.story.js
@@ -1,10 +1,41 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
+import TextInput from "@crave/farmblocks-input-text";
 
 import FilterPopover from ".";
 
-storiesOf("Filter Popover", module).add(
-  "Default",
-  withInfo()(() => <FilterPopover />)
-);
+storiesOf("Filter Popover", module)
+  .add(
+    "Default",
+    withInfo()(() => (
+      <FilterPopover
+        triggerLabel="Filter"
+        formTitle="Form Title"
+        formContent={
+          <div>
+            <TextInput label="Product" />
+            <TextInput label="Owner" />
+          </div>
+        }
+      />
+    ))
+  )
+  .add(
+    "With value set",
+    withInfo()(() => (
+      <FilterPopover
+        hasValue
+        triggerLabel="Filter"
+        formTitle="Form Title"
+        onFormSaveClick={action("onActionButtonClick")}
+        formContent={
+          <div>
+            <TextInput label="Product" value="Apple" name="product" />
+            <TextInput label="Owner" />
+          </div>
+        }
+      />
+    ))
+  );
