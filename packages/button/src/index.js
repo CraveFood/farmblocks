4606956425192bf4336e22f52d4e005efd239050
Felipe Gameiro <felipegameiro@gmail.com>
2018-09-19 16:31:10 -0300

    feat(Button): export colorTypes file
    affects: @crave/farmblocks-button
    
    ISSUES CLOSED: #555

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,3 +1,4 @@
 export { default } from "./Button";
 export { default as buttonTypes } from "./constants/buttonTypes";
 export { default as buttonSizes } from "./constants/buttonSizes";
+export { default as colorTypes } from "./constants/colorTypes";
