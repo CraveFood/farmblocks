57bb257fc492fba7ffd912ffb77f83ac29c8408d
Felipe Gameiro <felipegameiro@gmail.com>
2018-10-19 19:16:27 -0300

    test(DisableTooltip and withTooltip): fix broken tests
    affects: @crave/farmblocks-hoc-disabled-tooltip, @crave/farmblocks-hoc-withtooltip

--- packages/hoc-disabled-tooltip/src/disabledTooltip.test.js
+++ packages/hoc-disabled-tooltip/src/disabledTooltip.test.js
@@ -1,5 +1,4 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import Adapter from "enzyme-adapter-react-16";
 import { mount, configure } from "enzyme";
 
@@ -7,77 +6,16 @@ import disabledTooltip from ".";
 
 describe("disabledTooltip HOC", function() {
   configure({ adapter: new Adapter() });
-  test("default onMouseOver function returns null", function() {
-    const EnhancedInput = disabledTooltip("input");
-    const component = renderer.create(<EnhancedInput />);
-    const tree = component.toTree();
-    expect(tree.props.onMouseOver()).toBeNull();
-  });
-
-  test("default onMouseLeave function returns null", function() {
-    const EnhancedInput = disabledTooltip("input");
-    const component = renderer.create(<EnhancedInput />);
-    const tree = component.toTree();
-    expect(tree.props.onMouseLeave()).toBeNull();
-  });
-
-  test("onMouseOver property is called on mouse over", function() {
-    const onMouseOverMock = jest.fn();
-    const EnhancedInput = disabledTooltip("input");
-    const component = mount(
-      <EnhancedInput disabled onMouseOver={onMouseOverMock} />
-    );
-    const hitArea = component.find(".hit");
-    hitArea.simulate("mouseover", {});
-    expect(onMouseOverMock).toBeCalled();
-  });
-
-  test("onMouseLeave property is called on mouse leave", function() {
-    const onMouseLeaveMock = jest.fn();
-    const EnhancedInput = disabledTooltip("input");
-    const component = mount(
-      <EnhancedInput disabled onMouseLeave={onMouseLeaveMock} />
-    );
-    const hitArea = component.find(".hit");
-    hitArea.simulate("mouseleave", {});
-    expect(onMouseLeaveMock).toBeCalled();
-  });
 
   test("mouse events are passed to the wrapped component when not disabled", function() {
     const onClickMock = jest.fn();
-    const onMouseOverMock = jest.fn();
-    const onMouseLeaveMock = jest.fn();
-    const EnhancedInput = disabledTooltip("button");
-    const component = mount(
-      <EnhancedInput
-        onClick={onClickMock}
-        onMouseOver={onMouseOverMock}
-        onMouseLeave={onMouseLeaveMock}
-      />
-    );
-    const wrappedComponent = component.find("button");
-    wrappedComponent.simulate("click", {});
-    wrappedComponent.simulate("mouseover", {});
-    wrappedComponent.simulate("mouseleave", {});
-    expect(onMouseLeaveMock).toBeCalled();
-    expect(onMouseOverMock).toBeCalled();
-    expect(onClickMock).toBeCalled();
-  });
 
-  test("mouseover and mouseleave events should change showTooltip state on disabled fields", function() {
     const EnhancedInput = disabledTooltip("input");
-    const component = mount(<EnhancedInput disabled />);
-    const hitArea = component.find(".hit");
-    const defaultState = component.state();
+    const component = mount(<EnhancedInput onClick={onClickMock} />);
+    const wrappedComponent = component.find("input");
 
-    hitArea.simulate("mouseover", {});
-    const mouseOverState = component.state();
+    wrappedComponent.simulate("click", {});
 
-    hitArea.simulate("mouseleave", {});
-    const mouseLeaveState = component.state();
-
-    expect(defaultState.showTooltip).toBe(false);
-    expect(mouseOverState.showTooltip).toBe(true);
-    expect(mouseLeaveState.showTooltip).toBe(false);
+    expect(onClickMock).toBeCalled();
   });
 });
--- packages/hoc-withtooltip/src/withTooltip.test.js
+++ packages/hoc-withtooltip/src/withTooltip.test.js
@@ -9,23 +9,37 @@ configure({ adapter: new Adapter() });
 const InputWithTooltip = withTooltip("input");
 
 describe("withTooltip", () => {
-  it("should open tooltip on mouse over", () => {
-    const wrapper = mount(<InputWithTooltip />);
+  let onMouseOverMock, onMouseLeaveMock;
+
+  beforeEach(() => {
+    onMouseOverMock = jest.fn();
+    onMouseLeaveMock = jest.fn();
+  });
+
+  afterEach(() => {
+    onMouseOverMock.mockRestore();
+    onMouseLeaveMock.mockRestore();
+  });
+
+  it("should open tooltip on mouse over and call on mouse over function", () => {
+    const wrapper = mount(<InputWithTooltip onMouseOver={onMouseOverMock} />);
 
     wrapper.find(".hitArea").simulate("mouseover");
 
     expect(wrapper.state()).toEqual({ tooltipVisible: true });
+    expect(onMouseOverMock).toHaveBeenCalledTimes(1);
   });
 
-  it("should close tooltip on mouse leave", () => {
-    const wrapper = mount(<InputWithTooltip />);
-    const div = wrapper.find("div").first();
+  it("should close tooltip on mouse leave and call on mouse leave function", () => {
+    const wrapper = mount(<InputWithTooltip onMouseLeave={onMouseLeaveMock} />);
+    const div = wrapper.find(".hitArea");
 
     div.simulate("mouseover");
     expect(wrapper.state()).toEqual({ tooltipVisible: true });
 
     div.simulate("mouseleave");
     expect(wrapper.state()).toEqual({ tooltipVisible: false });
+    expect(onMouseLeaveMock).toHaveBeenCalledTimes(1);
   });
 
   it("should deactivate tooltip display and mouse events if disableTooltip is true", () => {
