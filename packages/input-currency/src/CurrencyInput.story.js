8ade4727af03a0eb04a0d014e0a8c2ea653b1c77
Vinicius Nakamura <hello@vnakamura.com>
2018-11-06 10:53:38 -0200

    test(input-currency): add stories
    affects: @crave/farmblocks-input-currency

--- packages/input-currency/src/CurrencyInput.story.js
+++ packages/input-currency/src/CurrencyInput.story.js
@@ -4,7 +4,33 @@ import { withInfo } from "@storybook/addon-info";
 
 import CurrencyInput from ".";
 
-storiesOf("Currency Input", module).add(
-  "Default",
-  withInfo()(() => <CurrencyInput />)
-);
+storiesOf("Currency Input", module)
+  .add("Default", withInfo()(() => <CurrencyInput />))
+  .add("With Prefix", withInfo()(() => <CurrencyInput prefix="$" />))
+  .add("With Suffix", withInfo()(() => <CurrencyInput suffix="KG" />))
+  .add(
+    "With Prefix and Suffix",
+    withInfo()(() => <CurrencyInput prefix="$" suffix="KG" />)
+  )
+  .add(
+    "With custom separators",
+    withInfo()(() => (
+      <CurrencyInput decimalSeparator="," thousandSeparator="." />
+    ))
+  )
+  .add(
+    "With custom decimal precision",
+    withInfo()(() => <CurrencyInput precision={4} />)
+  )
+  .add("Allow negative", withInfo()(() => <CurrencyInput allowNegative />))
+  .add("Disabled", withInfo()(() => <CurrencyInput disabled />))
+  .add(
+    "With Validation Messages",
+    withInfo()(() => (
+      <CurrencyInput validationMessages={["You need to fill this field."]} />
+    ))
+  )
+  .add(
+    "Custom margin of 50px",
+    withInfo()(() => <CurrencyInput margin="50px" />)
+  );
