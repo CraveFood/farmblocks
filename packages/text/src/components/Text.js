6a8363d5f182e84b8b19f8718e221e21c535b5bc
André L. C. Ferreira <alcf.2099@gmail.com>
2018-12-10 13:48:01 -0200

    fix(Text): accept any string on type property
    affects: @crave/farmblocks-text

--- packages/text/src/components/Text.js
+++ packages/text/src/components/Text.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import values from "object.values";
 import { fontSizes, fontTypes } from "@crave/farmblocks-theme";
 
 import Container from "../styledComponents/Text";
@@ -18,7 +17,7 @@ export default class Text extends React.Component {
   static propTypes = {
     children: PropTypes.node,
     size: PropTypes.number,
-    type: PropTypes.oneOf(values(fontTypes)),
+    type: PropTypes.string,
     align: PropTypes.oneOf(["left", "center", "right", "justify"]),
     lineHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     paragraph: PropTypes.bool,
