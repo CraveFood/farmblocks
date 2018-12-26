d149d54c5d6d552fb3c10238b2ae47d6dcb1dbf2
Felipe Gameiro <felipegameiro@gmail.com>
2018-12-26 12:50:00 -0200

    fix(Storybook): use action function from @storybook/addon-actions
    affects: @crave/farmblocks-map-balloon, @crave/farmblocks-popover, @crave/farmblocks-search-field

--- packages/popover/src/Popover.story.js
+++ packages/popover/src/Popover.story.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { storiesOf, action } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import Link from "@crave/farmblocks-link";
 import Text from "@crave/farmblocks-text";
 
