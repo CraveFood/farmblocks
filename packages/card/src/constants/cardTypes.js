58cc129e495a81f323e157f74760ccc614daa16e
Felipe Gameiro <felipegameiro@gmail.com>
2019-01-02 17:22:44 -0200

    fix(Card): fix cardTypes exports
    affects: @crave/farmblocks-card

--- packages/card/src/constants/cardTypes.js
+++ packages/card/src/constants/cardTypes.js
@@ -1,18 +1,18 @@
 import { colors } from "@crave/farmblocks-theme";
 
-const cardTypes = {
+export const cardTypes = {
   FEATURED: "FEATURED",
   NEUTRAL: "NEUTRAL",
   DEFAULT: "DEFAULT"
 };
 
-const cardTypeColors = {
+export const cardTypeColors = {
   [cardTypes.FEATURED]: colors.INDIGO_MILK_CAP,
   [cardTypes.NEUTRAL]: colors.SUGAR,
   [cardTypes.DEFAULT]: "#ffffff"
 };
 
-module.exports = {
+export default {
   cardTypes,
   cardTypeColors
 };
