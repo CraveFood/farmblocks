35feb1d2c1639ed033833fe0f5e9d6c9749f201c
André L. C. Ferreira <alcf.2099@gmail.com>
2019-01-07 11:41:13 -0200

    feat(test - drop commit): changed default alert type
    affects: @crave/farmblocks-alert

--- packages/alert/src/Alert.js
+++ packages/alert/src/Alert.js
@@ -22,7 +22,7 @@ class Alert extends React.Component {
   };
 
   static defaultProps = {
-    type: AlertTypes.NEWS,
+    type: AlertTypes.POSITIVE,
     dismissable: true,
     autoRemove: true,
     zIndex: DEFAULT_Z_INDEX,
