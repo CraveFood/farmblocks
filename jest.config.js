4260d4b1bf85f9c9f14aeb3e3575e1369e966018
Vinicius Nakamura <hello@vnakamura.com>
2019-02-04 17:43:41 -0200

    test(jest.config): add regenerator-runtime
    required for async/await tests to work on jest 22
    https://github.com/facebook/jest/issues/5698

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,25 @@
 const { argv } = process;
-const coveragePathIgnorePatterns = [".story.js", "lib/*"];
+
+const basicConfig = {
+  coveragePathIgnorePatterns: [".story.js", "lib/*"],
+  setupFiles: [
+    "./.storybook/jest/require.context.js",
+    "<rootDir>/node_modules/regenerator-runtime/runtime"
+    // ⬆️ for async/await tests to work in jest 22
+    // Ref.: https://github.com/facebook/jest/issues/5698
+  ]
+};
 
 const completeConfig = {
+  ...basicConfig,
   verbose: true,
-  coveragePathIgnorePatterns,
-  setupFilesAfterEnv: ["<rootDir>/jest.testFramework.js"],
-  setupFiles: ["./.storybook/jest/require.context.js"]
+  setupFilesAfterEnv: ["<rootDir>/jest.testFramework.js"]
 };
 
 const fastConfig = {
+  ...basicConfig,
   verbose: false,
   onlyChanged: true,
-  coveragePathIgnorePatterns,
   testPathIgnorePatterns: ["Storyshots.test.js"]
 };
 
