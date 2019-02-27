cdfe791b56fae87b93c01f59d314779fc60f1990
André L. C. Ferreira <alcf.2099@gmail.com>
2019-02-27 09:15:29 -0300

    chore(eslintrc): throw errors on prettier formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,13 @@ module.exports = {
     "standard",
     "eslint:recommended",
     "plugin:react/recommended",
-    "prettier"
+    "prettier" // Turns off all rules that are unnecessary or might conflict with Prettier.
   ],
-  plugins: ["react"]
+  plugins: [
+    "react",
+    "prettier" // Runs Prettier as an ESLint rule and reports differences as individual ESLint issues.
+  ],
+  rules: {
+    "prettier/prettier": "error" // Prettier rules must throw errors
+  }
 };
