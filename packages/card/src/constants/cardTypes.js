e3700dfc7aaabc099fee4408ab949e21e5690875
André L. C. Ferreira <alcf.2099@gmail.com>
2019-02-28 18:19:35 -0300

    fix(Card): lint
    affects: @crave/farmblocks-card

--- packages/card/src/constants/cardTypes.js
+++ packages/card/src/constants/cardTypes.js
@@ -3,16 +3,16 @@ import { colors } from "@crave/farmblocks-theme";
 export const cardTypes = {
   FEATURED: "FEATURED",
   NEUTRAL: "NEUTRAL",
-  DEFAULT: "DEFAULT"
+  DEFAULT: "DEFAULT",
 };
 
 export const cardTypeColors = {
   [cardTypes.FEATURED]: colors.INDIGO_MILK_CAP,
   [cardTypes.NEUTRAL]: colors.SUGAR,
-  [cardTypes.DEFAULT]: "#ffffff"
+  [cardTypes.DEFAULT]: "#ffffff",
 };
 
 export default {
   cardTypes,
-  cardTypeColors
+  cardTypeColors,
 };
