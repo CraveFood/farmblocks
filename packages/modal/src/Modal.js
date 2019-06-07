d3f4a11261a5ee94a85c097f9bf0fb0e83447533
Vinicius Nakamura <hello@vnakamura.com>
2019-06-07 12:45:34 -0300

    feat(modal): swap transition direction
    affects: @crave/farmblocks-modal

--- packages/modal/src/Modal.js
+++ packages/modal/src/Modal.js
@@ -60,9 +60,9 @@ const Modal = ({
     unique: true,
   });
   const slide = useTransition(isOpen, null, {
-    from: { transform: "translate3D(0, 50px, 0)" },
+    from: { transform: "translate3D(0, -50px, 0)" },
     enter: { transform: "translate3D(0, 0px, 0)" },
-    leave: { transform: "translate3D(0, -100px, 0)" },
+    leave: { transform: "translate3D(0, 50px, 0)" },
     unique: true,
   });
 
