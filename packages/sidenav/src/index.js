1ab6c9446fd65e17f0c8acd5396b8bd967e5ef2d
mateusstanki <darkerostanki@gmail.com>
2019-10-08 14:18:00 -0300

    feat(sidenav): export package components
    affects: @crave/farmblocks-sidenav

---
+++ packages/sidenav/src/index.js
@@ -0,0 +1,4 @@
+export { default, SideNavWithButtons } from "./SideNav";
+export { default as NavItem } from "./NavItem";
+export { default as NavButton, NavToggle, NavClose } from "./NavButton";
+export { default as Content } from "./Content";
