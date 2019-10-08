189ef511a2fd0821aa180a4da51a39f115a88a1c
Vinicius Nakamura <hello@vnakamura.com>
2019-10-08 10:35:29 -0300

    feat(input-phone): create script to generate a list of countries
    affects: @crave/farmblocks-input-phone
    
    it removes the unnecessary data from the word-countries json

---
+++ packages/input-phone/updateCountryList.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const countries = require("world-countries");
+const { getCountryCallingCode } = require("libphonenumber-js");
+
+const getNativeNames = (names, englishName) => {
+  const nativeNames = Array.from(
+    new Set(Object.keys(names).map(lang => names[lang].common)),
+  )
+    .filter(nativeName => nativeName !== englishName)
+    .join("; ");
+
+  return nativeNames;
+};
+
+const trimmedList = countries
+  .map(country => {
+    let callingCode;
+    try {
+      callingCode = getCountryCallingCode(country.cca2);
+    } catch (error) {
+      // Countries that don't have a calling code don't need to be on the list
+      return null;
+    }
+
+    return {
+      name: country.name.common,
+      nativeNames: getNativeNames(country.name.native, country.name.common),
+      flag: country.flag,
+      code: country.cca2,
+      callingCode,
+      altSpellings: country.altSpellings,
+    };
+  })
+  .filter(country => country !== null)
+  .sort((a, b) => a.name.localeCompare(b.name, "en"));
+
+fs.writeFileSync("./src/countries.json", JSON.stringify(trimmedList));
