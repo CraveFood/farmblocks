8f041fbd762d7d0ecb45af4ecad86c478a3f07cd
mateusstanki <darkerostanki@gmail.com>
2019-10-10 21:28:58 -0300

    feat(sidenav): Add mobile variant styles

--- packages/sidenav/src/SideNav.styled.js
+++ packages/sidenav/src/SideNav.styled.js
@@ -1,33 +1,71 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { colors } from "@crave/farmblocks-theme";
 import { transparentize } from "polished";
 
+const isFullWidth = variant => variant === "fullWidth";
+const isOverlay = variant => variant === "overlay";
+const isPush = variant => variant === "push";
+
+const sideNavFullWidth = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  ${({ expanded }) => expanded && "padding: 16px;"}
+  width: ${({ expanded }) => (expanded ? "100%" : "0px")};
+`;
+
+const sideNavOverlay = css`
+  width: ${({ expanded, expandedWidth }) => (expanded ? expandedWidth : "0px")};
+`;
+
 export const Sidebar = styled.nav`
-  * {
-    box-sizing: border-box;
-  }
+  box-sizing: border-box;
   position: fixed;
   bottom: 0;
   left: 0;
-  overflow-x: hidden;
-  transition: width 0.25s;
-  transition-timing-function: ease-in-out;
   top: ${({ offsetTop }) => offsetTop};
+  overflow-x: hidden;
+  transition: 0.25s;
+  transition-timing-function: ease-in-out;
   background-color: ${({ background }) => background};
   box-shadow: 0px 2px 2px 0px ${colors.GREY_32};
   z-index: ${({ zIndex }) => zIndex};
-  width: ${({ collapsedWidth }) => collapsedWidth};
-  ${({ expanded, expandedWidth }) => expanded && `width: ${expandedWidth};`}
+  width: ${({ expanded, collapsedWidth, expandedWidth }) =>
+    expanded ? expandedWidth : collapsedWidth};
+
+  @media screen and (min-width: 760px) {
+    ${({ variant }) => {
+      if (isOverlay(variant)) {
+        return sideNavOverlay;
+      }
+      if (isFullWidth(variant)) {
+        return sideNavFullWidth;
+      }
+      return "";
+    }}
+  }
+
+  @media screen and (max-width: 760px) {
+    ${({ mobileVariant }) => {
+      if (isOverlay(mobileVariant)) {
+        return sideNavOverlay;
+      }
+      if (isFullWidth(mobileVariant)) {
+        return sideNavFullWidth;
+      }
+      return "";
+    }}
+  }
 `;
 
 export const Item = styled.li`
   display: flex;
   align-items: center;
-  padding: 8px;
   list-style-type: none;
+  padding: 8px;
   cursor: pointer;
+  background-color: ${({ background }) => background};
   color: ${colors.GREY_48};
-  ${({ background }) => `background-color: ${background};`}
 
   .thumb-container {
     display: flex;
@@ -57,6 +95,43 @@ export const Item = styled.li`
     padding-left: 4px;
     border-left: 4px solid ${({ highlightColor }) => highlightColor};
   }
+
+  @media screen and (min-width: 760px) {
+    ${({ variant, highlightColor }) =>
+      isFullWidth(variant) &&
+      css`
+        border-radius: 90px;
+        padding: 8px 16px;
+
+        &:hover,
+        &.activated {
+          color: ${highlightColor};
+        }
+
+        &.activated {
+          padding-left: 16px;
+          border-left: none;
+        }
+      `}
+  }
+  @media screen and (max-width: 760px) {
+    ${({ mobileVariant, highlightColor }) =>
+      isFullWidth(mobileVariant) &&
+      css`
+        border-radius: 90px;
+        padding: 8px 16px;
+
+        &:hover,
+        &.activated {
+          color: ${highlightColor};
+        }
+
+        &.activated {
+          padding-left: 16px;
+          border-left: none;
+        }
+      `}
+  }
 `;
 
 export const IconButton = styled.div`
@@ -80,8 +155,82 @@ export const IconButton = styled.div`
   }
 `;
 
+export const ToggleButton = styled(IconButton)`
+  position: fixed;
+  top: 8px;
+  left: 8px;
+
+  @media screen and (min-width: 760px) {
+    z-index: ${({ variant, zIndex }) =>
+      isPush(variant) ? zIndex + 1 : zIndex - 1};
+  }
+
+  @media screen and (max-width: 760px) {
+    z-index: ${({ mobileVariant, zIndex }) =>
+      isPush(mobileVariant) ? zIndex + 1 : zIndex - 1};
+  }
+`;
+
+export const CloseButton = styled(IconButton)`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  display: flex;
+
+  @media screen and (min-width: 760px) {
+    ${({ variant, expanded }) =>
+      isPush(variant)
+        ? css`
+            display: none;
+          `
+        : css`
+            opacity: ${expanded ? 1 : 0};
+            transition: 0.25s;
+          `}
+  }
+  @media screen and (max-width: 760px) {
+    ${({ mobileVariant, expanded }) =>
+      isPush(mobileVariant)
+        ? css`
+            display: none;
+          `
+        : css`
+            opacity: ${expanded ? 1 : 0};
+            transition: 0.25s;
+          `}
+  }
+`;
+
+const contentOverlay = css`
+  transition: 0.3s;
+  ${({ expanded }) => expanded && `background-color:${colors.GREY_16};`};
+`;
+
+const contentPush = css`
+  transition: margin 0.25s;
+  transition-timing-function: ease-in-out;
+  margin-left: ${({ expanded, collapsedWidth, expandedWidth }) =>
+    expanded ? expandedWidth : collapsedWidth};
+`;
+
+export const Content = styled.div`
+  box-sizing: border-box;
+  ${({ offsetTop }) => offsetTop && `margin-top: ${offsetTop};`};
+
+  @media screen and (min-width: 760px) {
+    ${({ variant }) => isPush(variant) && contentPush};
+    ${({ variant }) => isOverlay(variant) && contentOverlay};
+  }
+
+  @media screen and (max-width: 760px) {
+    ${({ mobileVariant }) => isPush(mobileVariant) && contentPush};
+    ${({ mobileVariant }) => isOverlay(mobileVariant) && contentOverlay};
+  }
+`;
+
 export default {
   Sidebar,
   Item,
   IconButton,
+  Content,
 };
