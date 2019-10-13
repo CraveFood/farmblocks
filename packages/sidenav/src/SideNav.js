51ffcce3a0386733e173beeeac3a423d730d7cb5
mateusstanki <darkerostanki@gmail.com>
2019-10-13 20:06:34 -0300

    feat(sidenav): inject sidenav props into childs and add sidenav with buttons component
    affects: @crave/farmblocks-sidenav

--- packages/sidenav/src/SideNav.js
+++ packages/sidenav/src/SideNav.js
@@ -2,29 +2,73 @@ import React from "react";
 import PropTypes from "prop-types";
 import { colors } from "@crave/farmblocks-theme";
 
+import { PUSH } from "./constants/variants";
 import { Sidebar } from "./SideNav.styled";
+import { NavToggle, NavClose } from "./NavButton";
 
-const SideNav = React.memo(props => <Sidebar {...props} />);
+const SideNav = React.memo(({ children, ...props }) => {
+  const { highlightColor, variant, expanded, zIndex } = props;
+  return (
+    <Sidebar {...props}>
+      {React.Children.map(
+        children,
+        child =>
+          child &&
+          React.cloneElement(child, {
+            highlightColor,
+            variant,
+            expanded,
+            zIndex,
+          }),
+      )}
+    </Sidebar>
+  );
+});
+
+export const SideNavWithButtons = React.memo(
+  ({ children, onToggle, onClose, ...props }) => {
+    return (
+      <>
+        {onToggle && (
+          <NavToggle {...props} onClick={onToggle} active={!props.expanded} />
+        )}
+        <SideNav {...props}>
+          {onClose && <NavClose onClick={onClose} />}
+          {children}
+        </SideNav>
+      </>
+    );
+  },
+);
 
 SideNav.defaultProps = {
-  width: "300px",
   background: colors.SUGAR,
-  zIndex: 1,
+  zIndex: 100,
   offsetTop: "0",
   collapsedWidth: "56px",
   expandedWidth: "270px",
+  variant: PUSH,
   expanded: true,
 };
 
 SideNav.propTypes = {
   children: PropTypes.node,
-  width: PropTypes.string,
   background: PropTypes.string,
   zIndex: PropTypes.number,
   offsetTop: PropTypes.string,
   collapsedWidth: PropTypes.string,
   expandedWidth: PropTypes.string,
   expanded: PropTypes.bool,
+  variant: PropTypes.string,
+};
+
+SideNavWithButtons.defaultProps = {
+  ...SideNav.defaultProps,
+};
+
+SideNavWithButtons.propTypes = {
+  ...SideNav.propTypes,
+  highlightColor: PropTypes.string,
 };
 
 export default SideNav;
