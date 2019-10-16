6acfc58372e48f75b9eca156a3729cf0470760b0
mateusstanki <darkerostanki@gmail.com>
2019-10-16 14:18:12 -0300

    docs(sidenav): add stories without router and docs readme link
    affects: @crave/farmblocks-sidenav

--- packages/sidenav/README.md
+++ packages/sidenav/README.md
@@ -1,13 +1,13 @@
-
-
 # Farmblocks SideNav
 
-
+Sidebar navigation menu
 
 ## Installation
 
 ```
 npm install @crave/farmblocks-sidenav
+
+See [Docs](https://cravefood.github.io/farmblocks/?path=/story/sidenav--side-nav-push)
 ```
 
 ## License
--- packages/sidenav/src/SideNav.story.js
+++ packages/sidenav/src/SideNav.story.js
@@ -1,18 +1,119 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { colors } from "@crave/farmblocks-theme";
+import Text from "@crave/farmblocks-text";
 import { Switch, Route, NavLink, withRouter } from "react-router-dom";
 import StoryRouter from "storybook-react-router";
 import { useMediaQuery } from "react-responsive";
 
 import { SideNavWithButtons } from "./SideNav";
-import { FULLWIDTH, PUSH, OVERLAY } from "./constants/variants";
+import { FULLSCREEN, PUSH, OVERLAY } from "./constants/variants";
 import NavItem from "./NavItem";
 import Content from "./Content";
 
 const tabs = ["purveyor", "order", "search", "meat"];
 
-export const PushMenu = withRouter(({ location }) => {
+const SideNavPushComp = () => {
+  const [expanded, setExpanded] = useState(false);
+  const toggle = () => setExpanded(e => !e);
+
+  return (
+    <>
+      <SideNavWithButtons
+        expanded={expanded}
+        onToggle={toggle}
+        onClose={() => setExpanded(false)}
+      >
+        <NavHeader />
+        {tabs.map(tab => (
+          <NavItem key={tab} activated={tab === tabs[0]} icon={`wg-${tab}`}>
+            {tab}
+          </NavItem>
+        ))}
+      </SideNavWithButtons>
+
+      <Content expanded={expanded}>
+        <LoremBlock variant={PUSH} />
+      </Content>
+    </>
+  );
+};
+
+export const SideNavPush = () => <SideNavPushComp />;
+
+const SideNavOverlayComp = () => {
+  const [expanded, setExpanded] = useState(false);
+  const toggle = () => setExpanded(e => !e);
+  const collapse = () => setExpanded(false);
+
+  const handleClick = expanded ? collapse : undefined;
+
+  return (
+    <>
+      <SideNavWithButtons
+        expanded={expanded}
+        onToggle={toggle}
+        onClose={() => setExpanded(false)}
+        variant={OVERLAY}
+      >
+        <NavHeader />
+        {tabs.map(tab => (
+          <NavItem
+            key={tab}
+            activated={tab === tabs[0]}
+            icon={`wg-${tab}`}
+            onClick={handleClick}
+          >
+            {tab}
+          </NavItem>
+        ))}
+      </SideNavWithButtons>
+
+      <Content expanded={expanded} variant={OVERLAY} onClick={handleClick}>
+        <LoremBlock variant={OVERLAY} />
+      </Content>
+    </>
+  );
+};
+export const SideNavOverlay = () => <SideNavOverlayComp />;
+
+const SideNavFullScreenComp = () => {
+  const [expanded, setExpanded] = useState(false);
+  const toggle = () => setExpanded(e => !e);
+  const collapse = () => setExpanded(false);
+
+  const handleClick = expanded ? collapse : undefined;
+
+  return (
+    <>
+      <SideNavWithButtons
+        expanded={expanded}
+        onToggle={toggle}
+        onClose={() => setExpanded(false)}
+        variant={FULLSCREEN}
+      >
+        <NavHeader />
+        {tabs.map(tab => (
+          <NavItem
+            key={tab}
+            activated={tab === tabs[0]}
+            icon={`wg-${tab}`}
+            onClick={handleClick}
+          >
+            {tab}
+          </NavItem>
+        ))}
+      </SideNavWithButtons>
+
+      <Content expanded={expanded} variant={FULLSCREEN}>
+        <LoremBlock variant={FULLSCREEN} />
+      </Content>
+    </>
+  );
+};
+export const SideNavFullScreen = () => <SideNavFullScreenComp />;
+
+export const SideNavPushWithRouter = withRouter(({ location }) => {
   const [expanded, setExpanded] = useState(false);
   const toggle = () => setExpanded(e => !e);
 
@@ -33,14 +134,17 @@ export const PushMenu = withRouter(({ location }) => {
     </>
   );
 });
-PushMenu.story = {
+SideNavPushWithRouter.story = {
   decorators: [StoryRouter()],
 };
 
-export const overlayMenu = withRouter(({ location }) => {
+export const SideNavOverlayWithRouder = withRouter(({ location }) => {
   const [expanded, setExpanded] = useState(false);
   const toggle = () => setExpanded(e => !e);
+  const collapse = () => setExpanded(false);
+
   const highlightColor = colors.AVOCADO;
+  const handleClick = expanded ? collapse : undefined;
 
   return (
     <>
@@ -48,71 +152,59 @@ export const overlayMenu = withRouter(({ location }) => {
         expanded={expanded}
         variant={OVERLAY}
         onToggle={toggle}
-        onClose={() => setExpanded(false)}
+        onClose={collapse}
         highlightColor={highlightColor}
       >
         <NavHeader />
-        <NavItems
-          tabs={tabs}
-          location={location}
-          onClick={() => setExpanded(false)}
-        />
+        <NavItems tabs={tabs} location={location} onClick={collapse} />
       </SideNavWithButtons>
 
-      <Content
-        variant={OVERLAY}
-        expanded={expanded}
-        onClick={expanded ? () => setExpanded(false) : undefined}
-      >
+      <Content variant={OVERLAY} expanded={expanded} onClick={handleClick}>
         <NavRoutes tabs={tabs} />
       </Content>
     </>
   );
 });
-overlayMenu.story = {
+SideNavOverlayWithRouder.story = {
   decorators: [StoryRouter()],
 };
 
-// block scroll on full screen
-export const fullScreenMenu = withRouter(({ location }) => {
+export const SideNavFullScreenWithRouter = withRouter(({ location }) => {
   const [expanded, setExpanded] = useState(false);
   const toggle = () => setExpanded(e => !e);
+  const collapse = () => setExpanded(false);
 
   return (
     <>
       <SideNavWithButtons
         expanded={expanded}
-        variant={FULLWIDTH}
+        variant={FULLSCREEN}
         onToggle={toggle}
-        onClose={() => setExpanded(false)}
+        onClose={collapse}
       >
         <NavHeader />
-        <NavItems
-          tabs={tabs}
-          location={location}
-          onClick={() => setExpanded(false)}
-        />
+        <NavItems tabs={tabs} location={location} onClick={collapse} />
       </SideNavWithButtons>
 
-      <Content variant={FULLWIDTH} expanded={expanded}>
+      <Content variant={FULLSCREEN} expanded={expanded}>
         <NavRoutes tabs={tabs} />
       </Content>
     </>
   );
 });
-
-fullScreenMenu.story = {
+SideNavFullScreenWithRouter.story = {
   decorators: [StoryRouter()],
 };
 
-export const navWithJsMediaQuery = withRouter(({ location }) => {
+export const NavWithJsMediaQuery = withRouter(({ location }) => {
   const [expanded, setExpanded] = useState(false);
+  const toggle = () => setExpanded(e => !e);
+  const collapse = () => setExpanded(false);
   const isMobile = useMediaQuery({ maxWidth: 760 });
 
-  const toggle = () => setExpanded(e => !e);
-  const variant = isMobile ? FULLWIDTH : PUSH;
+  const variant = isMobile ? FULLSCREEN : PUSH;
   const sidebarColor = isMobile ? "white" : undefined;
-  const collapseAfterClick = isMobile ? () => setExpanded(false) : undefined;
+  const handleClick = isMobile ? collapse : undefined;
 
   return (
     <div>
@@ -122,14 +214,10 @@ export const navWithJsMediaQuery = withRouter(({ location }) => {
         onToggle={toggle}
         variant={variant}
         background={sidebarColor}
-        onClose={() => setExpanded(false)}
+        onClose={collapse}
         offsetTop="56px"
       >
-        <NavItems
-          tabs={tabs}
-          location={location}
-          onClick={collapseAfterClick}
-        />
+        <NavItems tabs={tabs} location={location} onClick={handleClick} />
       </SideNavWithButtons>
       <Content expanded={expanded} variant={variant} offsetTop="56px">
         <NavRoutes tabs={tabs} />
@@ -137,13 +225,11 @@ export const navWithJsMediaQuery = withRouter(({ location }) => {
     </div>
   );
 });
-
-navWithJsMediaQuery.story = {
+NavWithJsMediaQuery.story = {
   decorators: [StoryRouter()],
 };
 
 /* eslint-disable */
-
 const NavItems = ({ tabs, location, onClick, ...props }) => (
   <>
     {tabs.map(tab => (
@@ -190,12 +276,12 @@ const NavRoutes = ({ tabs }) => (
     <Switch>
       {[...tabs, "account"].map(tab => (
         <Route path={`/${tab}`} key={tab}>
-          <h1>{tab}</h1>
+          <Text upper fontWeight="title" size={32}>{tab}</Text>
           <Lorem />
         </Route>
       ))}
       <Route>
-        <h1>select a route</h1>
+        <Text upper fontWeight="title" size={32}>select a route</Text>
         <Lorem />
       </Route>
     </Switch>
@@ -229,10 +315,30 @@ const NavHeader = styled.div`
 `;
 
 const Lorem = () => (
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus
+  <div
+    style={{
+      margin: "16px 0",
+    }}
+  >
+  <Text>
+    Lorem iTextsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus
     molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias
     officiis assumenda officia quibusdam deleniti eos cupiditate dolore
     doloribus!
-  </p>
+  </Text>
+  </div>
+);
+
+const LoremBlock = ({variant}) => (
+  <div
+    style={{
+      margin: "0 24px",
+      padding: "24px",
+      height: "100vh",
+      boxSizing: "border-box",
+    }}
+  >
+    <Text upper fontWeight="title" size={32}>{variant}</Text>
+    <Lorem />
+  </div>
 );
