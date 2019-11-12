a4a6ccc212b7f036dc71ce2d7a3534ace55ab2f5
Vinicius Nakamura <hello@vnakamura.com>
2019-11-12 11:05:01 -0300

    feat(more-info): use new icons
    affects: @crave/farmblocks-more-info
    
    BREAKING CHANGE:
    removed the icon prop in favor of trigger

--- packages/more-info/src/components/MoreInfo.js
+++ packages/more-info/src/components/MoreInfo.js
@@ -4,43 +4,33 @@ import PropTypes from "prop-types";
 import Tooltip, { POSITIONS } from "@crave/farmblocks-tooltip";
 import Text from "@crave/farmblocks-text";
 import { fontSizes, fontTypes } from "@crave/farmblocks-theme";
+import { MdQuestion } from "@crave/farmblocks-icon";
 
 import StyledInfo from "../styledComponents/StyledInfo";
 
-const MoreInfo = ({
-  icon,
-  children,
-  text,
-  tooltipProps,
-  trigger,
-  ...props
-}) => {
+const MoreInfo = ({ children, text, tooltipProps, trigger, ...props }) => {
   const [tooltipVisible, setTooltipVisible] = useState(false);
 
   return (
     <StyledInfo {...props} tooltipProps={tooltipProps}>
-      <Text
-        fontWeight="title"
-        type={fontTypes.NEUTRAL}
-        size={fontSizes.MEDIUM}
-        className={`text ${tooltipVisible ? "hovered" : ""}`}
-      >
-        {text}
+      <Text fontWeight="title" type={fontTypes.NEUTRAL} size={fontSizes.MEDIUM}>
+        <span className={`text ${tooltipVisible ? "hovered" : ""}`}>
+          {text}
+        </span>
+        <Tooltip
+          trigger={trigger}
+          content={children}
+          className="icon"
+          onMouseOver={() => setTooltipVisible(true)}
+          onMouseLeave={() => setTooltipVisible(false)}
+          {...tooltipProps}
+        />
       </Text>
-      <Tooltip
-        trigger={trigger || <i className={icon} />}
-        content={children}
-        className="icon"
-        onMouseOver={() => setTooltipVisible(true)}
-        onMouseLeave={() => setTooltipVisible(false)}
-        {...tooltipProps}
-      />
     </StyledInfo>
   );
 };
 
 MoreInfo.propTypes = {
-  icon: PropTypes.string,
   children: PropTypes.node.isRequired,
   trigger: PropTypes.node,
   text: PropTypes.string,
@@ -49,7 +39,7 @@ MoreInfo.propTypes = {
 };
 
 MoreInfo.defaultProps = {
-  icon: "wg-question",
+  trigger: <MdQuestion />,
   tooltipProps: {
     positionX: POSITIONS.X.CENTER,
   },
