b1c2850e1628405f7b5f12e5e56482ddfbcbf251
Vinicius Nakamura <hello@vnakamura.com>
2020-06-18 10:51:28 -0300

    feat(table): create basic components
    BREAKING CHANGE: complete rewrite of the package.
    
    affects: @crave/farmblocks-table

--- packages/table/src/Table.story.js
+++ packages/table/src/Table.story.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import styled from "styled-components";
 
-import { Table } from "./index";
+import { Table, THead, TBody, TR, TH, TD, TDBold } from "./index";
 
 // eslint-disable-next-line no-unused-vars
 const fruits = [
@@ -11,4 +12,69 @@ const fruits = [
   { id: 3, name: "Coconut", price: "$ 2.30" },
 ];
 
-storiesOf("Table/Basic", module).add("Simple usage", () => <Table />);
+storiesOf("Table/Basic", module)
+  .add("Simple usage", () => (
+    <Table>
+      <THead>
+        <TR>
+          <TH>Fruit</TH>
+          <TH>Price</TH>
+        </TR>
+      </THead>
+      <TBody>
+        <TR>
+          <TD>Apple</TD>
+          <TDBold>$ 0.50</TDBold>
+        </TR>
+        <TR>
+          <TD>Banana</TD>
+          <TDBold>$ 0.60</TDBold>
+        </TR>
+        <TR>
+          <TD>Cherry</TD>
+          <TDBold>$ 9,999.99</TDBold>
+        </TR>
+        <TR>
+          <TD>Coconut</TD>
+          <TDBold>$ 2.30</TDBold>
+        </TR>
+      </TBody>
+    </Table>
+  ))
+  .add("Custom Cell", () => {
+    const TDRed = React.useMemo(
+      () =>
+        styled(TD)`
+          color: tomato;
+        `,
+      [],
+    );
+    return (
+      <Table>
+        <THead>
+          <TR>
+            <TH>Fruit</TH>
+            <TH>Price</TH>
+          </TR>
+        </THead>
+        <TBody>
+          <TR>
+            <TD>Apple</TD>
+            <TDRed>$ 0.50</TDRed>
+          </TR>
+          <TR>
+            <TD>Banana</TD>
+            <TDRed>$ 0.60</TDRed>
+          </TR>
+          <TR>
+            <TD>Cherry</TD>
+            <TDRed>$ 9,999.99</TDRed>
+          </TR>
+          <TR>
+            <TD>Coconut</TD>
+            <TDRed>$ 2.30</TDRed>
+          </TR>
+        </TBody>
+      </Table>
+    );
+  });
--- packages/table/src/components.styled.js
+++ packages/table/src/components.styled.js
@@ -1,6 +1,42 @@
 import styled from "styled-components";
+import { colors } from "@crave/farmblocks-theme";
+import Text from "@crave/farmblocks-text";
 
-// eslint-disable-next-line import/prefer-default-export
 export const Table = styled.table`
   border: none;
+  border-spacing: 0;
+`;
+
+export const THead = styled.thead`
+  background-color: ${colors.SUGAR};
+`;
+
+export const TBody = styled.tbody``;
+
+export const TR = styled.tr``;
+
+export const TH = styled(Text).attrs({
+  as: "th",
+})`
+  border: solid ${colors.GREY_08};
+  border-width: 1px 0;
+  padding: 8px 24px;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+`;
+
+export const TD = styled(Text).attrs({
+  as: "td",
+})`
+  border-bottom: 1px solid ${colors.GREY_08};
+  padding: 16px 24px;
+  font-size: 16px;
+  line-height: 24px;
+  color: ${colors.OYSTER};
+`;
+
+export const TDBold = styled(TD)`
+  font-weight: 700;
+  color: ${colors.CARBON};
 `;
