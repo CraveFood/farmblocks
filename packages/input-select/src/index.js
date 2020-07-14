a5ec68ddf5e72444d1aa014c9542dc3a4b8c4a3a
André L. C. Ferreira <alcf.2099@gmail.com>
2020-07-14 13:07:32 -0300

    refactor(input-select): refactor component to work with new hoc-input
    affects: @crave/farmblocks-input-select

--- packages/input-select/src/index.js
+++ packages/input-select/src/index.js
@@ -1,3 +1,2 @@
 export { default } from "./Select";
 export { default as Item } from "./components/Item";
-export { fontSizes } from "@crave/farmblocks-theme";
--- packages/input-select/src/styledComponents/TagsWrapper.js
+++ packages/input-select/src/styledComponents/TagsWrapper.js
@@ -6,10 +6,16 @@ export default styled.div`
   flex: 1;
 
   & > div {
-    margin: 8px 0 0 8px;
+    margin: 8px 0px 8px 8px;
   }
   & > div + input {
     flex: 1;
     min-width: 80px !important;
+  }
+
+  .wrapped {
+    border: none;
+    margin: 8px 16px;
+    cursor: pointer;
   }
 `;
