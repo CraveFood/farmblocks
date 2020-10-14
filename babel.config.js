7d6fb62d405002a7af63498cddab99c8b4d69823
Vinicius Nakamura <hello@vnakamura.com>
2020-10-14 11:12:35 -0300

    chore(babel): remove unused plugins

--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,8 @@ module.exports = api => {
   return {
     presets: ["@babel/preset-react"],
     plugins: [
-      // Stage 1 plugins
       "@babel/plugin-proposal-optional-chaining",
-      // Stage 2 plugins
-      ["@babel/plugin-proposal-decorators", { legacy: true }],
-      "@babel/plugin-proposal-function-sent",
-      "@babel/plugin-proposal-export-namespace-from",
-      "@babel/plugin-proposal-numeric-separator",
-      "@babel/plugin-proposal-throw-expressions",
-      // Stage 3 plugins
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-syntax-import-meta",
       ["@babel/plugin-proposal-class-properties", { loose: false }],
-      "@babel/plugin-proposal-json-strings",
       "babel-plugin-styled-components",
     ],
     env: {
